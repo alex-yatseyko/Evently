c575be034acdb8aeeababcc788400b3881a28c4b
Oleksandr Yatseiko <mac@Oleksandrs-MacBook-Air.local>
2023-12-16 11:30:56 -0500

    clerk & header added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='w-full border-b'>
+        <div className='wrapper flex items-center justify-between'>
+            <Link href='/' className='w-36'>
+                <Image src='/assets/images/logo.svg' alt='Evently' width={128} height={36}          />
+            </Link>
+
+            <SignedIn>
+                <nav className='md:flex-between hidden w-full max-w-xs'>
+                 <NavItems />
+                </nav>
+            </SignedIn>
+            
+            <div className='flex items-center justify-between'>
+                <SignedIn>
+                    <UserButton afterSignOutUrl='/'/>
+                    <MobileNav />
+                </SignedIn>
+                <SignedOut>
+                    <Button asChild className='rounded-full' size="lg">
+                        <Link href='/sign-in'>Login</Link>
+                    </Button>
+                </SignedOut>
+            </div>
+        </div>
+    </header>
   )
 }
 
